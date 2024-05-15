a0db1c231924224ac628c51021ef09fabad7d339
Depaula-Dev <roseli.dpaula@gmail.com>
2024-05-15 19:50:43 -0300

    feat: inserção de dados do usuario atraves de scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o número da sua Agência Bancária: ");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("Digite o número da sua Conta Corrente: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua Agência é " + numeroAgencia + ", Conta Corrente " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");    
+    
     }
 }
